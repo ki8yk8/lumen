62f4ff96def686a2e6b570f75b94b30cb442ef87
ki8yk8 <ki***@gmail.com>
2025-11-22 22:55:32 +0545

    adds level 2 framework

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Engine, { COLORS } from "./engine";
 import { registerLevel1Scene } from "./scenes/level1";
+import { registerLevel2Scene } from "./scenes/level2";
 
 const k = new Engine({
 	width: window.innerWidth,
@@ -11,5 +12,6 @@ const k = new Engine({
 k.loadSprite("player", "/sprites/player.png");
 
 registerLevel1Scene({ k });
+registerLevel2Scene({ k });
 
-k.go("level-1");
+k.go("level-2");
--- src/scenes/level2.js
+++ src/scenes/level2.js
@@ -1,5 +1,28 @@
-export function registerLevel2Scene({k}) {
+import Ray from "../objects/ray";
+import Target from "../objects/target";
+import Mirror from "../objects/mirror";
+import Player from "../objects/player";
+import { vec2 } from "../engine/vec2";
+
+export function registerLevel2Scene({ k }) {
 	k.scene("level-2", () => {
-		
-	})
+		Ray({
+			k,
+			angle: 0,
+			speed: 100,
+			spawnrate: 0.2,
+			pos: vec2(100, 200),
+		});
+		Target({
+			k,
+			pos: k.vec2(500, 500),
+			target: 2,
+			onComplete: () => k.go("level-2"),
+		});
+
+		Mirror({ k, angle: 15, pos: vec2(600, 200) });
+
+		// player should be at the top
+		Player({ k, pos: vec2(100, 100) });
+	});
 }
