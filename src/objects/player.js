be48fdd71d46ce99189076be1ef376974c6cd5bb
ki8yk8 <ki***@gmail.com>
2025-11-22 18:36:59 +0545

    creates rotating head with movement

--- src/objects/player.js
+++ src/objects/player.js
@@ -1,16 +1,36 @@
-export default function Player({ k }) {
+export default function Player({ k, rotation_speed = 90, player_speed = 300 }) {
 	const player = k.add([
 		k.rect(80, 80, { radius: 40 }),
 		k.pos(100, 100),
+		k.rotate(0),
 		k.color("ORANGE"),
 		k.area(),
+		{
+			is_rotating: true,
+		},
 	]);
 
 	const player_head = k.add([
-		k.rect(30, 30, {radius: [0, 30, 30, 0]}),
-		k.pos(player.pos.sub(10, 0)),
-		k.anchor("left"),
-	])
+		k.rect(30, 30, { radius: [0, 30, 30, 0] }),
+		k.pos(player.pos),
+		k.rotate(0),
+		k.anchor("center"),
+	]);
+
+	k.onUpdate(() => {
+		if (player.is_rotating) {
+			player.angle += rotation_speed * k.dt;
+			player_head.angle += rotation_speed * k.dt;
+		} else {
+			player.is_rotating = true;
+		}
+	});
+
+	k.onKeyDown(" ", () => {
+		player.moveBy(k.vec2(player_speed * k.dt, 0).rotate(player.angle));
+		player_head.moveBy(k.vec2(player_speed * k.dt, 0).rotate(player.angle));
+		player.is_rotating = false;
+	});
 
 	return player;
 }
