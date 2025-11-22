37e83c57dd26a519e364d563985a616bf4dcddba
ki8yk8 <ki***@gmail.com>
2025-11-22 21:03:59 +0545

    stylizes the player to be more like a player

--- src/objects/player.js
+++ src/objects/player.js
@@ -17,20 +17,32 @@ export default function Player({
 	]);
 
 	const player_head = k.add([
-		k.rect(30, 30, { radius: [0, 30, 30, 0] }),
-		k.pos(player.pos),
+		k.rect(40, 35, { radius: [20, 30, 30, 20] }),
+		k.pos(player.pos.add(2, 0)),
 		k.rotate(0),
 		k.anchor("center"),
 	]);
+	k.add([
+		k.rect(40, 10, { radius: [40, 40, 0, 0] }),
+		k.pos(player.pos.sub(0, player.height / 2)),
+		k.color("PURPLE"),
+		k.anchor("center"),
+	]);
+	k.add([
+		k.rect(40, 10, { radius: [0, 0, 40, 40] }),
+		k.pos(player.pos.add(0, player.height / 2)),
+		k.color("PURPLE"),
+		k.anchor("center"),
+	]);
 
-	k.onUpdate(() => {
-		if (player.is_rotating) {
-			player.angle += rotation_speed * k.dt;
-			player_head.angle += rotation_speed * k.dt;
-		} else {
-			player.is_rotating = true;
-		}
-	});
+	// k.onUpdate(() => {
+	// 	if (player.is_rotating) {
+	// 		player.angle += rotation_speed * k.dt;
+	// 		player_head.angle += rotation_speed * k.dt;
+	// 	} else {
+	// 		player.is_rotating = true;
+	// 	}
+	// });
 
 	k.onKeyDown(" ", () => {
 		player.moveBy(k.vec2(player_speed * k.dt, 0).rotate(player.angle));
