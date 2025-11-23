2d0b9e02fefcdf7dcbbda06c234dea6d9644e133
ki8yk8 <ki***@gmail.com>
2025-11-23 15:37:05 +0545

    stylizes the level2 game

--- src/scenes/level2.js
+++ src/scenes/level2.js
@@ -24,7 +24,8 @@ export function registerLevel2Scene({ k }) {
 		Mirror({ k, angle: 15, pos: vec2(600, 200) });
 		Mirror({ k, angle: 40, pos: vec2(800, 500) });
 
-		Obstacle({ k, width: 150, pos: k.vec2(500, 400), angle: 15 });
+		Obstacle({ k, width: 150, pos: k.vec2(500, 400), angle: 0 });
+		Obstacle({k, width: 50, pos: k.vec2(500+75, 400+20), angle: 90});
 
 		// player should be at the top
 		Player({ k, pos: vec2(100, 100) });
