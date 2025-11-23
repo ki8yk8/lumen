7c9ccee16784774f01bce4bc2af235987a4d11f8
ki8yk8 <ki***@gmail.com>
2025-11-23 21:53:23 +0545

    runs heavy task only at first

--- src/engine/components/text.js
+++ src/engine/components/text.js
@@ -19,51 +19,55 @@ export function text(
 		width: 0,
 		height: size,
 		text,
-		textSize: size,
-		lineHeight,
-		font,
+		textOpts: { textSize: size, lineHeight, font, lines: [] },
 		maxWidth,
+		loaded: false,
 		draw(ctx, e) {
-			ctx.font = `${e.textSize}px ${e.font}`;
+			ctx.font = `${e.textOpts.textSize}px ${e.textOpts.font}`;
 			ctx.fillStyle = rgbToHex(e.color);
 
-			// to handle the word wrap if maxWidth is given
-			const words = e.text.split(" ");
-			let line = "";
-			const lines = [];
-			const widths = [];
+			if (!e.loaded) {
+				// to handle the word wrap if maxWidth is given
+				const words = e.text.split(" ");
+				let line = "";
+				const lines = [];
+				const widths = [];
 
-			for (let i = 0; i < words.length; i++) {
-				const test_line = line === "" ? `${words[i]} ` : `${line}${words[i]} `;
-				const test_width = ctx.measureText(test_line).width;
+				for (let i = 0; i < words.length; i++) {
+					const test_line =
+						line === "" ? `${words[i]} ` : `${line}${words[i]} `;
+					const test_width = ctx.measureText(test_line).width;
 
-				if (test_width > e.maxWidth && line !== "") {
+					if (test_width > e.maxWidth && line !== "") {
+						lines.push(line);
+						widths.push(ctx.measureText(line).width);
+						line = `${words[i]} `;
+					} else {
+						line = test_line;
+					}
+				}
+
+				if (line !== "") {
 					lines.push(line);
 					widths.push(ctx.measureText(line).width);
-					line = `${words[i]} `;
-				} else {
-					line = test_line;
 				}
+
+				const block_width = widths.length ? Math.max(...widths) : 0;
+				const line_height = e.textOpts.lineHeight ?? e.textOpts.textSize * 1.2;
+				const block_height = line_height * lines.length;
+
+				e.loaded = true;
+				e.width = block_width;
+				e.height = block_height;
+				e.textOpts.lineHeight = line_height;
+				e.textOpts.lines = lines;
 			}
-
-			if (line !== "") {
-				lines.push(line);
-				widths.push(ctx.measureText(line).width);
-			}
-
-			const block_width = widths.length ? Math.max(...widths) : 0;
-			const line_height = e.lineHeight ?? e.textSize * 1.2;
-			const block_height = line_height * lines.length;
-
-			e.width = block_width;
-			e.height = block_height;
-			e.lineHeight = line_height;
 
 			const anchored_pos = convertBasedOnAnchor(
 				e.pos.x,
 				e.pos.y,
-				block_width,
-				block_height,
+				e.width,
+				e.height,
 				e.anchor
 			);
 
@@ -78,11 +82,11 @@ export function text(
 			// handles the scaling
 			ctx.scale(e.scale.x, e.scale.y);
 
-			for (let i = 0; i < lines.length; i++) {
+			for (let i = 0; i < e.textOpts.lines.length; i++) {
 				ctx.fillText(
-					lines[i].trimEnd(),
+					e.textOpts.lines[i].trimEnd(),
 					anchored_pos.x - e.pos.x,
-					anchored_pos.y - e.pos.y + i * line_height
+					anchored_pos.y - e.pos.y + i * e.textOpts.lineHeight
 				);
 			}
 
