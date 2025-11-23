f10407d95bb9c973ea3c50749ee5c8b30d3db99a
ki8yk8 <ki***@gmail.com>
2025-11-23 20:24:58 +0545

    adds main menu

--- src/scenes/mainmenu.js
+++ src/scenes/mainmenu.js
@@ -10,6 +10,7 @@ export default function registerMainMenuScene({ k }) {
 			"logo",
 		]);
 
+		// animating how the logo looks
 		k.animate(logo, "angle", [0, -5, 0, 5, 0], 1);
 		k.animate(
 			logo,
@@ -17,5 +18,61 @@ export default function registerMainMenuScene({ k }) {
 			[k.vec2(1), k.vec2(1.1), k.vec2(1), k.vec2(0.9), k.vec2(1)],
 			2
 		);
+		k.animate(
+			logo,
+			"pos",
+			[logo.pos, logo.pos.add(10, 0), logo.pos, logo.pos.add(-10, 0), logo.pos],
+			1
+		);
+
+		// adding the menu items
+		const menu_items = ["Start Game", "Instructions"];
+		const menu_items_objs = [];
+		let index = 0;
+
+		menu_items.forEach((item, index) => {
+			const item_bg = k.add([
+				k.rect(450, 80),
+				k.color("GREEN"),
+				k.pos(k.width() / 2, logo.pos.y + 200 + 100 * index),
+			]);
+
+			const item_text = k.add([
+				k.text(item, { size: 48 }),
+				k.color("WHITE"),
+				k.pos(item_bg.pos),
+			]);
+			menu_items_objs.push([item_bg, item_text]);
+		});
+
+		renderItem(menu_items_objs, 0, true);
+		k.onKeyPressed("ArrowDown", () => {
+			index = Math.min(index + 1, menu_items.length - 1);
+			renderItem(menu_items_objs, index, true);
+		});
+
+		k.onKeyPressed("ArrowUp", () => {
+			index = Math.max(0, index - 1);
+			renderItem(menu_items_objs, index, true);
+		});
+
+		function renderItem(objects, index, active = true) {
+			console.log(index);
+			const [item_bg, item_text] = objects[index];
+
+			item_bg.scale = active ? k.vec2(1.1) : k.vec2(1.0);
+			item_bg.color = active ? k.color("ORANGE").color : k.color("GREEN").color;
+			item_bg.angle = active ? 2 : 0;
+			item_text.scale = item_bg.scale;
+			item_text.angle = item_bg.angle;
+
+			if (active) {
+				for (let i = 0; i < objects.length; i++) {
+					if (i === index) continue;
+
+					renderItem(objects, i, false);
+				}
+			}
+		}
 	});
 }
